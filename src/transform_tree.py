f97bbafa856ff57e78a64a3a8dd48ae7cfc3f20f
Thomas Cohn <cohnt@umich.edu>
2022-06-12 23:07:18 -0400

    Switch from autograd to jax.

--- src/transform_tree.py
+++ src/transform_tree.py
@@ -1,16 +1,18 @@
-import autograd.numpy as np
+import jax.numpy as np
+from jax import jacfwd
+
 class TransformTreeNode():
-	def __init__(self, parent, psi, J, J_dot, f, M, space_dim):
+	def __init__(self, parent, psi, J, f, M, space_dim):
 		# parent is another TransformTreeNode, or None if this node is the root
 		# psi is a function mapping from the configuration space of the parent node to this node
-		# J is the Jacobian of psi, and J_dot is its derivative
+		# J is the Jacobian of psi (if set to None, it will be computed with jax)
 		# f is the desired force map
 		# M is the intertia matrix
 		# space_dim is the dimension of the configuration space
 		self.parent = parent
 		self.children = []
 		self.psi = psi
-		self.J = J
+		self.J = jacfwd(psi) if J is None else J
 		self.f = f
 		self.M = M
 		self.x = np.zeros((space_dim,1))
@@ -45,7 +47,7 @@ class TransformTreeNode():
 		x = self.x
 		x_dot = self.x_dot
 		for child in self.children:
-			self.f = self.f + child.J(x).T @ (child.f(x, x_dot) - (child.M(x, x_dot) @ grad(child.J)(x) @ x_dot))
+			self.f = self.f + child.J(x).T @ (child.f(x, x_dot) - (child.M(x, x_dot) @ jacfwd(child.J)(x) @ x_dot))
 			self.M = self.M + child.J(x).T @ child.M(x, x_dot) @ child.J(x)
 
 	def resolve(self):
